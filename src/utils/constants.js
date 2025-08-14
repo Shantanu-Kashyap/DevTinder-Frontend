aa8eda563ee81f4cb687c877f3ca43289f403ac4
Shantanu-Kashyap <shantanukumarsko@gmail.com>
2025-08-14 16:19:31 +0530

    config changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,5 @@
-
 const isLocal = window.location.hostname === "localhost";
 
 export const BASE_URL = isLocal
-  ? import.meta.env.VITE_LOCAL_BASE_URL
-  : import.meta.env.VITE_AWS_BASE_URL;
+  ? "/api"   // dev -> use vite proxy
+  : "/api";  // prod -> Nginx will forward /api to backend
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,20 @@
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
     port: 5173,
     proxy: {
+      // Only for development, so frontend can call backend without CORS issues
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3000', // Local backend
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+        rewrite: (path) => path.replace(/^\/api/, ''), 
+      },
+    },
+  },
 });
