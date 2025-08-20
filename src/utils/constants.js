10afe80c6bd146c0607f724ef38b025eaf7c7bb8
Shantanu-Kashyap <shantanukumarsko@gmail.com>
2025-08-20 14:47:43 +0530

    FEAT: Updated BASE_URL for Nginx setup

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,5 @@ const host = window.location.hostname;
 const isLocal = host === "localhost" || host === "127.0.0.1";
 
 export const BASE_URL = isLocal
-  ? "http://localhost:3000"        // local dev
-  : "http://13.53.90.173:3000";    // EC2 backend
+  ? "http://localhost:3000/api" 
+  : "/api"; 
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,11 +2,9 @@ import io from "socket.io-client";
 import { BASE_URL } from "./constants";
 
 export const createSocketConnection = () => {
-  if (location.hostname === "localhost") {
+  if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
     return io(BASE_URL);
+  } else {
+    return io(); 
   }
-  // else {
-  //   return io("/", { path: "/api/socket.io" });
-  // }
-  
 };
